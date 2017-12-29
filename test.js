413e745077dec2c9d241ffdaaaa8818cd16f8842
OyaKata <hdszk2002@yahoo.co.jp>
2017-12-29 20:16:57 +0900

    calcBefore

--- test.js
+++ test.js
@@ -8,33 +8,43 @@ document.body.appendChild( renderer.domElement );
 
 var axesHelper = new THREE.AxesHelper( 5 );
 scene.add( axesHelper );
-var pos = [
+
+//edge
+var edgePos = [
 	new THREE.Vector3(1,1,1),
 	new THREE.Vector3(2,1,2),
-	new THREE.Vector3(0,0,2)
 ];
+var col =[0xffff00,0xff00ff];
 
-var col =[0xffff00,0xff00ff,0x808080];
+var edgeGeometry = new THREE.Geometry();
+edgeGeometry.vertices[0] = new THREE.Vector3(0,0,0);
+edgeGeometry.colors[0] = new THREE.Color(0xffffff);
 
-var geometry = new THREE.Geometry();
-geometry.vertices[0] = new THREE.Vector3(0,0,0);
-geometry.colors[0] = new THREE.Color(0xffffff);
-
-var point= new Array();
-for(var key in pos){
-	point[key] = new THREE.Points(geometry,new THREE.PointsMaterial({color:col[key],size:0.1,vertexColors:true}));
-	scene.add(point[key]);
-	point[key].position.copy(pos[key]);
+var edgePoint= new Array();
+for(var key in edgePos){
+	edgePoint[key] = new THREE.Points(edgeGeometry,new THREE.PointsMaterial({color:col[key],size:0.1,vertexColors:true}));
+	scene.add(edgePoint[key]);
+	edgePoint[key].position.copy(edgePos[key]);
 }
-var t=0;
+
+//movongPoint
+var point=new THREE.Points(edgeGeometry,new THREE.PointsMaterial({color:0x808080,size:0.1,vertexColors:true}));
+scene.add(point);
+point.position.copy(edgePos[0]);
+var pos=new Array(10);
+const N=10;
+for(var i=0;i<10;i++){
+	var t=i/N;
+	pos[i]=new THREE.Vector3();
+	pos[i].addVectors ( edgePos[0].clone().multiplyScalar ( 1-t ),edgePos[1].clone().multiplyScalar ( t ) );
+}
+
+var cnt=0;
 function animate() {
-	if(t>1)t=0;
-	pos[2].addVectors ( pos[0].clone().multiplyScalar ( 1-t ),pos[1].clone().multiplyScalar ( t ) );
-	point[2].position.copy(pos[2])
+	if(cnt==10)cnt=0;
+	point.position.copy(pos[cnt])
 	requestAnimationFrame( animate );
 	renderer.render( scene, camera );
-	t += 0.1;
-
-	
+	cnt++;	
 }
 animate();
