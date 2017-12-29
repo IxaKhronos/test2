548d0ef044d1dd1e5255943ebe0684721dc18bbf
OyaKata <hdszk2002@yahoo.co.jp>
2017-12-29 19:56:16 +0900

    2PointGo

--- test.js
+++ test.js
@@ -1,29 +1,40 @@
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 var	controls = new THREE.OrbitControls(camera);
-
+camera.position.z = 5;
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var materials = new Array();
-//var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-materials.push(new THREE.MeshBasicMaterial( { color: 0xff0000 } ));
-materials.push(new THREE.MeshBasicMaterial( { color: 0xffff00 } ));
-materials.push(new THREE.MeshBasicMaterial( { color: 0xff00ff } ));
-materials.push(new THREE.MeshBasicMaterial( { color: 0x00ffff } ));
-materials.push(new THREE.MeshBasicMaterial( { color: 0x00ff00 } ));
-materials.push(new THREE.MeshBasicMaterial( { color: 0x0000ff } ));
-var cube = new THREE.Mesh( geometry, materials );
-scene.add( cube );
+var axesHelper = new THREE.AxesHelper( 5 );
+scene.add( axesHelper );
+var pos = [
+	new THREE.Vector3(1,1,1),
+	new THREE.Vector3(2,1,2),
+	new THREE.Vector3(0,0,2)
+];
 
-camera.position.z = 5;
+var col =[0xffff00,0xff00ff,0x808080];
 
+var geometry = new THREE.Geometry();
+geometry.vertices[0] = new THREE.Vector3(0,0,0);
+geometry.colors[0] = new THREE.Color(0xffffff);
+
+var point= new Array();
+for(var key in pos){
+	point[key] = new THREE.Points(geometry,new THREE.PointsMaterial({color:col[key],size:0.1,vertexColors:true}));
+	scene.add(point[key]);
+	point[key].position.copy(pos[key]);
+}
+var t=0;
 function animate() {
-	cube.rotation.x+=0.05;
-	cube.rotation.y+=0.1;
+	if(t>1)t=0;
+	pos[2].addVectors ( pos[0].clone().multiplyScalar ( 1-t ),pos[1].clone().multiplyScalar ( t ) );
+	point[2].position.copy(pos[2])
 	requestAnimationFrame( animate );
 	renderer.render( scene, camera );
+	t += 0.1;
+
+	
 }
 animate();
