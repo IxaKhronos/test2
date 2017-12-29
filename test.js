04518fc57f7c2269fbee82d2f270da904913e6f9
OyaKata <hdszk2002@yahoo.co.jp>
2017-12-30 07:14:39 +0900

    2beje

--- test.js
+++ test.js
@@ -13,8 +13,9 @@ scene.add( axesHelper );
 var edgePos = [
 	new THREE.Vector3(1,1,1),
 	new THREE.Vector3(2,1,2),
+	new THREE.Vector3(2,0,1)
 ];
-var col =[0xffff00,0xff00ff];
+var col =[0xffff00,0xff00ff,0x00ffff];
 
 var edgeGeometry = new THREE.Geometry();
 edgeGeometry.vertices[0] = new THREE.Vector3(0,0,0);
@@ -28,41 +29,70 @@ for(var key in edgePos){
 }
 
 //movongPoint
-var point=new THREE.Points(edgeGeometry,new THREE.PointsMaterial({color:0x808080,size:0.1,vertexColors:true}));
-scene.add(point);
-point.position.copy(edgePos[0]);
+var point=[
+	new THREE.Points(edgeGeometry,new THREE.PointsMaterial({color:0x808080,size:0.1,vertexColors:true})),
+	new THREE.Points(edgeGeometry,new THREE.PointsMaterial({color:0x808080,size:0.1,vertexColors:true})),
+	new THREE.Points(edgeGeometry,new THREE.PointsMaterial({color:0x808080,size:0.1,vertexColors:true}))
+];
+for(var key in point) scene.add(point[key]);
+
+point[0].position.copy(edgePos[0]);
+point[1].position.copy(edgePos[1]);
+point[2].position.copy(edgePos[0]);
 const N=10;
-var pos=new Array(N+1);
+var pos=[new Array(N+1),new Array(N+1),new Array(N+1)];
 for(var i=0;i<N+1;i++){
 	var t=i/N;
-	pos[i]=new THREE.Vector3();
-	pos[i].addVectors ( edgePos[0].clone().multiplyScalar ( 1-t ),edgePos[1].clone().multiplyScalar ( t ) );
+	pos[0][i]=new THREE.Vector3();
+	pos[0][i].addVectors ( edgePos[0].clone().multiplyScalar ( 1-t ),edgePos[1].clone().multiplyScalar ( t ) );
+	pos[1][i]=new THREE.Vector3();
+	pos[1][i].addVectors ( edgePos[1].clone().multiplyScalar ( 1-t ),edgePos[2].clone().multiplyScalar ( t ) );
+	pos[2][i]=new THREE.Vector3();
+	pos[2][i].addVectors ( pos[0][i].clone().multiplyScalar ( 1-t ),pos[1][i].clone().multiplyScalar ( t ) );
 }
+
 //line
-var geometry = new THREE.BufferGeometry();
-var positions = new Float32Array((N+1) * 3);
-geometry.addAttribute('position', new THREE.BufferAttribute(positions, 3));
-var array = geometry.attributes.position.array;
-for(var i in pos){
-	console.log(i)
-	array[i*3]=pos[i].x;
-	array[i*3+1]=pos[i].y;
-	array[i*3+2]=pos[i].z;
-	console.log(array[i*3])
+var lineGeometry = [
+new THREE.BufferGeometry(),
+new THREE.BufferGeometry(),
+new THREE.BufferGeometry()
+]
+var lMaterial =[
+	new THREE.LineBasicMaterial({ color: 0x990000, linewidth : 1}),
+	new THREE.LineBasicMaterial({ color: 0x990000, linewidth : 1}),
+	new THREE.LineBasicMaterial({ color: 0x999900, linewidth : 1})
+]
+var positions =new Array(3);
+var line = new Array(3);
+for(var key in lineGeometry){
+	positions[key]=new Float32Array((N+1) * 3);
+	lineGeometry[key].addAttribute('position', new THREE.BufferAttribute(positions[key], 3));
+	var array = lineGeometry[key].attributes.position.array;
+	for(var i in pos[key]){
+		array[i*3]=pos[key][i].x;
+		array[i*3+1]=pos[key][i].y;
+		array[i*3+2]=pos[key][i].z;
+	}
+	line[key] = new THREE.Line(lineGeometry[key], lMaterial[key]);
+	scene.add(line[key]);
 }
-console.log(geometry)
-//geometry.addGroup(0, 1, 0);
-var lMaterial = new THREE.LineBasicMaterial({ color: 0x990000, linewidth : 1});
-var line = new THREE.Line(geometry, lMaterial);
-scene.add(line);
 
 var cnt=0;
+var dcnt=0;
+const DN=10
 function animate() {
 	if(cnt>N)cnt=0;
-	point.position.copy(pos[cnt])
-	line.geometry.setDrawRange(0, cnt);
+	for(var key in point){
+		point[key].position.copy(pos[key][cnt])
+		line[key].geometry.setDrawRange(0, cnt+1);
+	}
 	requestAnimationFrame( animate );
 	renderer.render( scene, camera );
-	cnt++;	
+	if(dcnt==DN){
+		cnt++;
+		dcnt=0;
+	}else{
+		dcnt++;
+	}
 }
 animate();
